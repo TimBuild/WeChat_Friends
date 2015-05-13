9eebe9dc79c0306fd5110054eee8a5f1847a9a7a
gracieYe <505830661@qq.com>
2015-05-13 09:32:16 +0800

    Add AddMomentActivity

---
+++ WeChatDemo/src/com/matrix/wechat/activity/AddMomentActivity.java
@@ -0,0 +1,13 @@
+package com.matrix.wechat.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddMomentActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
